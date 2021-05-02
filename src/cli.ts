6a4e9c66007a2a08893822449a532fe68b220fee
unlomtrois <vadim.kuz02@gmail.com>
2021-05-02 15:50:00 +0300

    add cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,17 @@
-import commander from "commander";
+import { Command } from "commander";
 import { execSync } from "child_process";
 import chalk from "chalk";
-import Suggest from "./suggest";
+// import Suggest from "./suggest";
 
 const ctx = new chalk.Instance({ level: 1 });
 
-/**
- * Модуль CLI
- */
-const cli = new commander.Command();
+const cli = new Command();
 
-/**
- * инициализация полей (опций)
- */
+cli.name("node-hh-parser").version("1.0.0");
+
+// инициализация полей (опций)
+
 cli
-  .option<number>(
-    "-l, --limit <number>",
-    "ограничение по поиску",
-    parseFloat,
-    2000
-  )
   .option(
     "-A, --all",
     "выполнить все остальные комманды автоматически",
@@ -30,59 +22,59 @@ cli
     "название территории поиска или индекс",
     "Россия"
   )
-  .option("-L, --language <язык>", "язык локализации", "RU")
-  .option("-C, --cluster", "поиск по кластерам")
   .option("-S, --silent", "не выводить информацию в консоль");
 
-/**
- * инициализация команд (операций)
- */
-// cli
-//   .command("search <text>")
-//   .description("поиск вакансий по полю text")
-//   .action(async (text: string) => {
-//     const area = await Suggest.area(cli.area, cli.language);
+// инициализация команд (операций)
 
-//     await io.search({
-//       text: text,
-//       area: area,
-//       limit: cli.limit,
-//       cluster: cli.cluster ?? cli.all ?? false,
-//     });
+cli
+  .command("search <text>")
+  .description("поиск вакансий по полю text")
+  .action(async (text: string) => {
+    // const area = await Suggest.area(cli.area, cli.language);
 
-//     if (cli.all) {
-//       setTimeout(() => {
-//         console.log(ctx.yellow("\n>"), "node cli.js get-full");
-//         execSync("node cli.js get-full", { stdio: "inherit" });
+    console.log(text, cli.opts());
+    // await search({
+    //   text: text,
+    //   area: area,
+    //   limit: cli.limit,
+    //   cluster: cli.cluster ?? cli.all ?? false,
+    // });
 
-//         console.log(ctx.yellow("\n>"), "node cli.js prepare");
-//         execSync("node cli.js prepare", { stdio: "inherit" });
+    if (cli.opts().all) {
+      setTimeout(() => {
+        console.log(ctx.yellow("\n>"), "node cli.js get-full");
+        execSync("node cli.js get-full", { stdio: "inherit" });
 
-//         console.log(ctx.yellow("\n>"), "node cli.js analyze");
-//         execSync("node cli.js analyze", { stdio: "inherit" });
-//       }, 1000);
-//     }
-//   });
+        console.log(ctx.yellow("\n>"), "node cli.js prepare");
+        execSync("node cli.js prepare", { stdio: "inherit" });
 
-// cli
-//   .command("get-full")
-//   .description("получает полное представление вакансий")
-//   .action(() => {
-//     io.getFullVacancies(cli.limit);
-//   });
+        console.log(ctx.yellow("\n>"), "node cli.js analyze");
+        execSync("node cli.js analyze", { stdio: "inherit" });
+      }, 1000);
+    }
+  });
 
-// cli
-//   .command("prepare")
-//   .description("подготовить полные вакансии, очистить их от ненужных полей")
-//   .action(() => {
-//     io.prepare();
-//   });
+cli
+  .command("get-full")
+  .description("получает полное представление вакансий")
+  .action(() => {
+    // io.getFullVacancies(cli.limit);
+  });
 
-// cli
-//   .command("analyze")
-//   .description("проанализировать полученные данные")
-//   .action(() => {
-//     io.analyze();
-//   });
+cli
+  .command("prepare")
+  .description("подготовить полные вакансии, очистить их от ненужных полей")
+  .action(() => {
+    // io.prepare();
+  });
 
-// cli.run();
+cli
+  .command("analyze")
+  .description("проанализировать полученные данные")
+  .action(() => {
+    // io.analyze();
+  });
+
+cli.parse(process.argv);
+
+export default cli;
